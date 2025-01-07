331f7396ff8b04945b8e4f8e672191a39e798e74
Wayne Workman <wayne@theworkmans.us>
2025-01-07 15:55:02 -0600

    Put scripts into root's home directory.

--- install.sh
+++ install.sh
@@ -8,8 +8,8 @@ fi
 
 cp deadman.py /deadman.py
 cp deadman.service /etc/systemd/system/deadman.service
-cp mount_data.sh /path/to/mount_data.sh
-cp unmount_data.sh /path/to/unmount_data.sh
+cp mount_data.sh ~/mount_data.sh
+cp unmount_data.sh ~/unmount_data.sh
 
 # Adjust ownership & permissions (recommended)
 chown root:root /deadman.py
@@ -18,11 +18,11 @@ chmod 700 /deadman.py
 chown root:root /etc/systemd/system/deadman.service
 chmod 644 /etc/systemd/system/deadman.service
 
-chown root:root /path/to/mount_data.sh
-chmod 700 /path/to/mount_data.sh
+chown root:root ~/mount_data.sh
+chmod 700 ~/mount_data.sh
 
-chown root:root /path/to/unmount_data.sh
-chmod 700 /path/to/unmount_data.sh
+chown root:root ~/unmount_data.sh
+chmod 700 ~/unmount_data.sh
 
 systemctl daemon-reload
 systemctl enable deadman
